f3981e175e7d2868cfcae0edafd33e71f1ee090f
Roth <82582846+RothGorham@users.noreply.github.com>
2023-10-27 00:14:27 -0300

    base

---
+++ funcoes.h
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+typedef struct {
+    int prioridade;
+    // Mudei o char para wchar_t, para que o programa aceite caracteres especiais
+    wchar_t categoria[100];
+    wchar_t descricao[300];
+} Tarefa;
+
+typedef struct {
+    Tarefa tarefas[100];
+    int qtd;
+} ListaDeTarefas;
+
+int criarTarefa(ListaDeTarefas *lt);
+int deletarTarefa(ListaDeTarefas *lt);
+int listarTarefa(ListaDeTarefas lt);
+
+void printMenu();
+// fwrite
+int salvarLista(ListaDeTarefas lt, char nome[]);
+// fread
+int carregarLista(ListaDeTarefas *lt, char nome[]);
