553993ac1818276b37c254d374ddc53d1d09cd79
Roth <82582846+RothGorham@users.noreply.github.com>
2023-11-10 14:02:28 -0300

    mudar tarefa logica

--- lista.c
+++ lista.c
@@ -4,7 +4,71 @@
 #include <locale.h>
 #include <string.h>
 
-// Cria uma nova tarefa e adiciona na lista
+int mudarTarefa(ListaDeTarefas *lt) {
+    // Verifica se a lista de tarefas está vazia
+    if (lt->qtd <= 0) {
+        printf("Lista de tarefas vazia.\n");
+        return 1;
+    }
+
+    printf("Mudar tarefa\nDigite a posição da tarefa (1 a 100):");
+    int pos;
+    printf("Digite a posição da tarefa (1 a 100): ");
+    scanf("%d", &pos);
+    pos--;
+
+    // Verifica se a posição é válida
+    if (pos < 0 || pos >= lt->qtd) {
+        printf("Posição inválida.\n");
+        return 1;
+    }
+
+    int opcao;
+    printf("Escolha o campo a ser alterado:\n");
+    printf("1 - Nome\n");
+    printf("2 - Categoria\n");
+    printf("3 - Descrição\n");
+    printf("4 - Prioridade\n");
+    printf("5 - Status\n");
+    printf("Digite a opção desejada: ");
+    scanf("%d", &opcao);
+
+    // buffer
+    while (getchar() != '\n');
+
+    // menu para o escolher as opcoes
+    switch (opcao) {
+        case 1:
+            printf("Digite o novo nome da tarefa: ");
+            fgets(lt->tarefas[pos].nome, sizeof(lt->tarefas[pos].nome), stdin);
+            break;
+        case 2:
+            printf("Digite a nova categoria da tarefa: ");
+            fgets(lt->tarefas[pos].categoria, sizeof(lt->tarefas[pos].categoria), stdin);
+            break;
+        case 3:
+            printf("Digite a nova descrição da tarefa: ");
+            fgets(lt->tarefas[pos].descricao, sizeof(lt->tarefas[pos].descricao), stdin);
+            break;
+        case 4:
+            printf("Digite a nova prioridade da tarefa (de 1 a 10): ");
+            scanf("%d", <->tarefas[pos].prioridade);
+            break;
+        case 5:
+            printf("Digite o novo status da tarefa (1 para completo, 2 para em andamento, 3 para não iniciado): ");
+            scanf("%d", <->tarefas[pos].status);
+            break;
+        default:
+            printf("Opção inválida.\n");
+            return 1;
+    }
+
+    return 0;
+}
+
+
+
+
 int criarTarefa(ListaDeTarefas *lt) {
     if (lt->qtd < 100) {
         printf("Criar tarefa:\n");
@@ -12,17 +76,41 @@ int criarTarefa(ListaDeTarefas *lt) {
         // Limpando o buffer do teclado antes de ler a categoria
         while (getchar() != '\n');
 
-        wprintf(L"Digite a categoria (até 100 caracteres): ");
-        fgetws(lt->tarefas[lt->qtd].categoria, sizeof(lt->tarefas[lt->qtd].categoria), stdin);
+        printf("Digite o nome da tarefa (até 100 caracteres): ");
+        fgets(lt->tarefas[lt->qtd].nome, sizeof(lt->tarefas[lt->qtd].nome), stdin);
+
+        // Remover a quebra de linha do nome da tarefa (se existir)
+        size_t nome_len = strlen(lt->tarefas[lt->qtd].nome);
+        if (lt->tarefas[lt->qtd].nome[nome_len - 1] == '\n') {
+            lt->tarefas[lt->qtd].nome[nome_len - 1] = '\0';
+        }
+
+        printf("Digite a categoria (até 100 caracteres): ");
+        fgets(lt->tarefas[lt->qtd].categoria, sizeof(lt->tarefas[lt->qtd].categoria), stdin);
+
+        // Remover a quebra de linha da categoria (se existir)
+        size_t categoria_len = strlen(lt->tarefas[lt->qtd].categoria);
+        if (lt->tarefas[lt->qtd].categoria[categoria_len - 1] == '\n') {
+            lt->tarefas[lt->qtd].categoria[categoria_len - 1] = '\0';
+        }
 
         // Limpando o buffer do teclado antes de ler a descrição
         while (getchar() != '\n');
 
-        wprintf(L"Digite a descrição (até 300 caracteres): ");
-        fgetws(lt->tarefas[lt->qtd].descricao, sizeof(lt->tarefas[lt->qtd].descricao), stdin);
+        printf("Digite a descrição (até 300 caracteres): ");
+        fgets(lt->tarefas[lt->qtd].descricao, sizeof(lt->tarefas[lt->qtd].descricao), stdin);
+
+        // Remover a quebra de linha da descrição (se existir)
+        size_t descricao_len = strlen(lt->tarefas[lt->qtd].descricao);
+        if (lt->tarefas[lt->qtd].descricao[descricao_len - 1] == '\n') {
+            lt->tarefas[lt->qtd].descricao[descricao_len - 1] = '\0';
+        }
 
         printf("Digite a prioridade (de 1 a 10): ");
         scanf("%d", &lt->tarefas[lt->qtd].prioridade);
+
+        // Limpando o buffer do teclado antes de ler o status
+        while (getchar() != '\n');
 
         printf("Digite o status (1 para completo, 2 para em andamento, 3 para não iniciado): ");
         scanf("%d", &lt->tarefas[lt->qtd].status);
@@ -30,7 +118,7 @@ int criarTarefa(ListaDeTarefas *lt) {
         lt->qtd++;
         return 0;
     } else {
-        wprintf(L"Lista de tarefas está cheia.\n");
+        printf("Lista de tarefas está cheia.\n");
         return 1;
     }
 }
@@ -69,33 +157,34 @@ int listarTarefa(ListaDeTarefas lt) {
     printf("\n");
 
     if (lt.qtd == 0) {
-        wprintf(L"A lista de tarefas está vazia.\n");
-        return 1; 
+        printf("A lista de tarefas está vazia.\n");
+        return 1;
     }
 
     for (int i = 0; i < lt.qtd; i++) {
         printf("Tarefa %d\n", i + 1);
-        wprintf(L"Categoria: %s", lt.tarefas[i].categoria);
-        wprintf(L"Descrição: %ls", lt.tarefas[i].descricao);
-        printf("Prioridade: %d\n", lt.tarefas[i].prioridade);
-        printf("Status: %d\n", lt.tarefas[i].status);  // mostrar o status 
+        printf("Nome: %s\n", lt.tarefas[i].nome);
+        printf("Categoria: %s\n", lt.tarefas[i].categoria);
+        printf("Descrição: %s\n", lt.tarefas[i].descricao);
+        printf("Status: %d\n", lt.tarefas[i].status);
         printf("\n");
     }
 
-    return 0; 
+    return 0;
 }
 
 
 // Mostra o menu
-void printMenu(){
-    setlocale(LC_ALL, "Portuguese");
+void printMenu() {
     printf("Menu\n");
     printf("1 - Criar tarefa\n");
     printf("2 - Deletar tarefa\n");
     printf("3 - Listar tarefas\n");
+    printf("4 - Mudar tarefa\n");  
     printf("0 - Sair\n");
-    wprintf(L"Digite a opção desejada: ");
+    printf("Digite a opção desejada: ");  
 }
+
 
 // Salva a lista de tarefas em um arquivo
 // fwrite
